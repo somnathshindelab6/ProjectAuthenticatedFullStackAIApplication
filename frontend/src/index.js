e71f6dd0de657d83b567afad9ca096a6cce18758
somnathshindelab4 <somnathshindelab4@gmail.com>
2026-07-18 15:06:01 +0530

    all

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+const PUBLIC_URL = process.env.PUBLIC_URL || '/ProjectAuthenticatedFullStackAIApplication'
+
 const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(
